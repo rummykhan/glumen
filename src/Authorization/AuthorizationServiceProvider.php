2a497f07003857b8ad590845adf86d6fe4d06c11
rummykhan <rehan_manzoor@outlook.com>
2017-10-21 15:32:33 +0400

    Changed the name to AuthorizationServiceProvider

--- src/Authorization/AuthorizationServiceProvider.php
+++ src/Authorization/AuthorizationServiceProvider.php
@@ -4,15 +4,13 @@ namespace Glumen\Authorization;
 
 use Illuminate\Support\ServiceProvider;
 
-class AuthServiceProvider extends ServiceProvider
+class AuthorizationServiceProvider extends ServiceProvider
 {
 
     public function register()
     {
-
         $this->app->singleton('authorization', function ($app) {
             return new Authorization($app);
         });
-
     }
 }
