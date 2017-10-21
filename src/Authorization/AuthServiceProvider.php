6b1aa8a6d7726c7ad19215ef76ae6fd6520ef0a4
rummykhan <rehan_manzoor@outlook.com>
2017-10-21 15:07:16 +0400

    Added Min useful Authorization

---
+++ src/Authorization/AuthServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Glumen\Authorization;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+
+        $this->app->singleton('authorization', function ($app) {
+            return new Authorization($app);
+        });
+
+    }
+}
